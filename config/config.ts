135c2fd0e9b804f8558d52a19adf1c2d1e1abe63
Facundo <facundoveliz9@gmail.com>
2022-08-31 23:31:16 -0300

    database setup

---
+++ config/config.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+// TODO: check if i should delete not used variables like port/secret
+const config = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  secret: process.env.JWT_PRIVATE_KEY,
+  dialect: 'mariadb'
+}
+
+export default config
--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 import routes from './routes'
+import { sequelize } from './models'
 import { errorHandler, notFound } from './middleware/errorHandlerMiddleware'
 
 const app = express()
@@ -17,6 +18,14 @@ app.use(routes)
 
 const port = process.env.PORT ?? 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.')
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err)
+  })
 
 app.use(notFound)
 app.use(errorHandler)
