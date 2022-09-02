f080f009047a1a8d3838bc68043ae163594d0cfd
Facundo <facundoveliz9@gmail.com>
2022-09-01 22:43:57 -0300

    changed eslint config from standard to airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,41 @@
 module.exports = {
   env: {
+    browser: true,
     es2021: true,
     node: true,
-    jest: true
   },
-  extends: 'standard-with-typescript',
-  overrides: [],
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json'
   },
-  rules: {}
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'consistent-return': 'off',
+    'no-underscore-dangle': 'off',
+    semi: ['error', 'never'],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+    '@typescript-eslint/no-namespace': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
 }
