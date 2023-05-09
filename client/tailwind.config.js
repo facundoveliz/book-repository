ed98ae52ba07485b6f779adb6f1669c83a15b158
Facundo Veliz <facundoveliz9@gmail.com>
2023-05-09 16:28:31 -0300

    added global variables

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // TODO: change this names
+      colors: {
+        background: "#0b1622",
+        "background-variant": "#101b27",
+
+        primary: "#09a0de",
+        "primary-variant": "#16adeb",
+
+        secondary: "#e13333",
+        "secondary-variant": "#df3131",
+      },
+      gridTemplateColumns: {
+        "auto-fit": "repeat(auto-fit, minmax(12rem, 1fr))",
+      },
+      display: ["group-hover"],
+    },
   },
   plugins: [],
 };
